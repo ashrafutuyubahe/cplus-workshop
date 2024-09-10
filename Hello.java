e377b63761aa5764622a9870ed920e2a3af17015
Ashrafu <ashraftuyubahe001@gmail.com>
2024-09-10 11:19:00 +0200

    added console java program

---
+++ Hello.java
@@ -0,0 +1,15 @@
+ class Hello{
+
+public static void main( String parameters[]){
+
+ int firstNumber=6;
+ int secondNumber=10;
+ int sum=firstNumber +secondNumber;
+
+System.out.println("hello "+ parameters[0] + " here is the answer: "+sum);
+
+
+}
+
+
+}
