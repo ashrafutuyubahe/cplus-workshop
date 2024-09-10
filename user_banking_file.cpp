976add71ba7a9a5f3c591071e428749e1cc38c6a
Ashrafu <ashraftuyubahe001@gmail.com>
2024-09-10 07:08:33 +0200

    fixing a bug of opening a file

--- user_banking_file.cpp
+++ user_banking_file.cpp
@@ -3,49 +3,71 @@
 #include<string.h>
 # define MAX_LENGHT 40
 
+  struct fileDetails{
+  	  char userName[40]; 	  
+  	   double accountNumber; 	
+  };
 
 
 
 
- void writeToFile( File *fptr, struct *fileDetails){
+ void writeToFile( FILE *fptr, struct fileDetails *fileDetails){
+ 	  if(fptr==NULL){
+ 	  	 printf("Error opening file!\n");
+        return;
+	   }
   	   
-  struct fileDetails storeFileDetails;  
-  fprintf(fptr,storeFileDetails);  
+  
+    fprintf(" user name : %s  ",fileDetails->userName);
+    fprintf("  account number : %d  ",fileDetails->accountNumber);   
   fclose(fptr);
   
  	
  }
  
  
- void readAccountDetailsFile( struct *fileDetails){
+ void readAccountDetailsFile( struct fileDetails *fileDetails){
+ 	
  	   struct fileDetails dataFromAccount_detailsfile; 
+ 	   
+ 	   
  	File FileOpenPtr= fopen("account_details.txt","r");
  	
- 	fgets(dataFromAccount_detailsfile,100,dataFromAccount_detailsfile);
+ 	 if(FileOpenPtr==NULL){
+ 	  	 printf("Error opening file!\n");
+        return;
+	   }
  	
- 	char retrievedUserName[50]=  dataFromAccount_detailsfile.userName;
  	
- 	double retrievedAccountNumber= dataFromAccount_detailsfile.accountNumber;
+          fscanf("user name: %s",dataFromAccount_detailsfile.userName);   
+		   fscanf(" account number: %s",dataFromAccount_detailsfile.accountNumber);    // 	fgets(dataFromAccount_detailsfile,100,dataFromAccount_detailsfile);
  	
- 	printf(" your user name is  %s   and your account number is %d",retrievedUserName,retrievedAccountNumber);
+ 	printf(" your user name is  %s   and your account number is %d",dataFromAccount_detailsfile.userName,dataFromAccount_detailsfile.accountNumber);
 	 	
    fclose(FileOpenPtr);
     
  	
  }
-
-  void printOnConsole();
-  
-  struct fileDetails{
-  	  char userName[40]; 	  
-  	   double accountNumber; 	
-  };
-  
  
   
   
-  
 
+void createAccount(char *userName, double *accountNumber, struct fileDetails *fileDetails){
+ 
+	
+    printf("Enter your User Name:  \n");
+    scanf("%s",userName);
+   
+    
+       printf("Enter your Account Number: \n");
+     scanf("%lf",accountNumber);
+
+      
+     printf("you have successfully created account\n %s and %lf  are your account credentials and all data are documented in a file  \n",userName,*accountNumber); 
+
+
+ }
+ 
 int main(){
   char userName[MAX_LENGHT];
   int userInput;
@@ -81,27 +103,6 @@ void printOnConsole(){
 	
 }
 
-void createAccount(char *userName, double *accountNumber, struct *fileDetails){
- 
-	
-    printf("Enter your User Name:  \n");
-    scanf("%s",userName);
-   
-    
-       printf("Enter your Account Number: \n");
-     scanf("%lf",accountNumber);
-     
-       struct fileDetails details;
-       
-       details.accountNumber= *accountNumber;
-       details.userName= *userName;   
-    
-      
-     printf("you have successfully created account\n %s and %lf  are your account credentials and all data are documented in a file  \n",userName,*accountNumber); 
-
-
- }
- 
  
  
 
