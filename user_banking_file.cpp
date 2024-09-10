55a5ae88f462b405565eff75e7ba620d4e9ced47
Ashrafu <ashraftuyubahe001@gmail.com>
2024-09-10 07:59:06 +0200

    finished the program

--- user_banking_file.cpp
+++ user_banking_file.cpp
@@ -1,109 +1,85 @@
- 
-#include<stdio.h>
-#include<string.h>
-# define MAX_LENGHT 40
+#include <stdio.h>
+#include <string.h>
 
-  struct fileDetails{
-  	  char userName[40]; 	  
-  	   double accountNumber; 	
-  };
+#define MAX_LENGTH 40
 
+struct fileDetails {
+    char userName[MAX_LENGTH];
+    double accountNumber;
+};
 
-
-
- void writeToFile( FILE *fptr, struct fileDetails *fileDetails){
- 	  if(fptr==NULL){
- 	  	 printf("Error opening file!\n");
+void writeToFile(FILE *fptr, struct fileDetails *fileDetails) {
+    if (fptr == NULL) {
+        printf("Error opening file!\n");
         return;
-	   }
-  	   
-  
-    fprintf(" user name : %s  ",fileDetails->userName);
-    fprintf("  account number : %d  ",fileDetails->accountNumber);   
-  fclose(fptr);
-  
- 	
- }
- 
- 
- void readAccountDetailsFile( struct fileDetails *fileDetails){
- 	
- 	   struct fileDetails dataFromAccount_detailsfile; 
- 	   
- 	   
- 	File FileOpenPtr= fopen("account_details.txt","r");
- 	
- 	 if(FileOpenPtr==NULL){
- 	  	 printf("Error opening file!\n");
+    }
+
+    fprintf(fptr, "user name: %s\n", fileDetails->userName);
+    fprintf(fptr, "account number: %.2lf\n", fileDetails->accountNumber);
+    fclose(fptr);
+}
+
+void readAccountDetailsFile(struct fileDetails *fileDetails) {
+    struct fileDetails dataFromAccount_detailsfile;
+    FILE *FileOpenPtr = fopen("account_details.txt", "r");
+
+    if (FileOpenPtr == NULL) {
+        printf("Error opening file!\n");
         return;
-	   }
- 	
- 	
-          fscanf("user name: %s",dataFromAccount_detailsfile.userName);   
-		   fscanf(" account number: %s",dataFromAccount_detailsfile.accountNumber);    // 	fgets(dataFromAccount_detailsfile,100,dataFromAccount_detailsfile);
- 	
- 	printf(" your user name is  %s   and your account number is %d",dataFromAccount_detailsfile.userName,dataFromAccount_detailsfile.accountNumber);
-	 	
-   fclose(FileOpenPtr);
-    
- 	
- }
- 
-  
-  
+    }
 
-void createAccount(char *userName, double *accountNumber, struct fileDetails *fileDetails){
- 
-	
-    printf("Enter your User Name:  \n");
-    scanf("%s",userName);
-   
-    
-       printf("Enter your Account Number: \n");
-     scanf("%lf",accountNumber);
+    fscanf(FileOpenPtr, "user name: %s", dataFromAccount_detailsfile.userName);
+    fscanf(FileOpenPtr, "account number: %lf", &dataFromAccount_detailsfile.accountNumber);
 
-      
-     printf("you have successfully created account\n %s and %lf  are your account credentials and all data are documented in a file  \n",userName,*accountNumber); 
+    printf("Your user name is %s and your account number is %.2lf\n",
+           dataFromAccount_detailsfile.userName, dataFromAccount_detailsfile.accountNumber);
 
-
- }
- 
-int main(){
-  char userName[MAX_LENGHT];
-  int userInput;
-  double accountNumber;
- 
-
-printOnConsole();
-
-FILE *fptr=  fopen("account_details.txt","w");
-
-
-switch(userInput){
-	
-	case 1:
-		createAccount(&userName,&accountNumber);
-     	writeToFile(fptr,&fileDetails);
-		break;
-	case 1:				
-	  readAccountDetailsFile();
-		break;
-	default:
-	  printf("invalid choice");
-}
-	
-	
-	return 0;
+    fclose(FileOpenPtr);
 }
 
-void printOnConsole(){
-	 printf("1.Create account \n");
-	 printf("2.view account details \n"); 
-	
-	
+void printOnConsole() {
+    printf("1. Create account\n");
+    printf("2. View account details\n");
 }
 
- 
- 
+void createAccount(char *userName, double *accountNumber, struct fileDetails *fileDetails) {
+    printf("Enter your User Name:\n");
+    scanf("%s", userName);
 
+    printf("Enter your Account Number:\n");
+    scanf("%lf", accountNumber);
 
+    strcpy(fileDetails->userName, userName);
+    fileDetails->accountNumber = *accountNumber;
+
+    printf("You have successfully created an account\n%s and %.2lf are your account credentials and all data are documented in a file\n",
+           userName, *accountNumber);
+}
+
+int main() {
+    char userName[MAX_LENGTH];
+    int userInput;
+    double accountNumber;
+    struct fileDetails fileDetails;
+
+    printOnConsole();
+
+    printf("Enter your choice: ");
+    scanf("%d", &userInput);
+
+    FILE *fptr = fopen("account_details.txt", "w");
+
+    switch (userInput) {
+        case 1:
+            createAccount(userName, &accountNumber, &fileDetails);
+            writeToFile(fptr, &fileDetails);
+            break;
+        case 2:
+            readAccountDetailsFile(&fileDetails);
+            break;
+        default:
+            printf("Invalid choice\n");
+    }
+
+    return 0;
+}
