bf138a26d00837d610b2eb571d833a2757e508ec
Ashrafu <ashraftuyubahe001@gmail.com>
2024-09-17 06:49:58 +0200

    added another console program

---
+++ y1cat_program.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+	
+	int number;
+	
+	for(number=1;number<= 10;number++){
+		
+	if(number ==5)
+	continue;	
+	
+	if(number==9)
+		break;
+	 printf(" %d",number);
+
+	
+}
+
+return 0;
+}
